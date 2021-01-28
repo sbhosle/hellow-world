2010cd2aec13d7b7fbbfa9cbfc7cff92314b047f
sbhosle <sbhosle01@yahoo.com>
2021-01-28 21:21:58 +0530

    Created Functions for Exercise 1 and 2
    added functions draw_circles and draw_polygon

--- main.py
+++ main.py
@@ -9,17 +9,29 @@ def get_random_color():
     return r, g, b
 
 
+def draw_circles():
+    no_of_circles = 36
+    angle = 0
+    increment = 360 / no_of_circles
+    for i in range(no_of_circles):
+        painter.color(get_random_color())
+        painter.setheading(angle)
+        painter.circle(100)
+        angle += increment
+
+
+def draw_polygons():
+    painter.forward(100)
+    angle = 90
+    painter.setheading(angle)
+
+
 turtle.colormode(255)
 painter = Turtle()
 painter.shape('arrow')
-no_of_circles = 36
-angle = 0
-increment = 360/no_of_circles
-for i in range(no_of_circles):
-    painter.color(get_random_color())
-    painter.setheading(angle)
-    painter.circle(100)
-    angle += increment
+
+draw_circles()
 
 screen = Screen()
 screen.exitonclick()
+
